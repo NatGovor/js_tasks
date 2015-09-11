aa0dbac5cda6871fc01459625b7ea845940bcb96
NatGovor <ngovor93@gmail.com>
2015-09-11 18:11:31 +0300

    started pseudo-classical pattern implementation

--- Inheritance/sushi_pseudoclassical.js
+++ Inheritance/sushi_pseudoclassical.js
@@ -7,7 +7,7 @@ if (typeof Object.create !== 'function') {
 }
 
 // Basic product
-var Product = function (name, price) {
+var Product = function(name, price) {
 	this.name = name;
 	this.price = price;
 };
@@ -17,5 +17,50 @@ Product.prototype.getName = function() {
 Product.prototype.getPrice = function() {
 	return this.price;
 };
+// end basic product
+
+var Fish = function (name, price) {
+	this.name = name;
+	this.price = price;
+};
+Fish.prototype = new Product();
+Fish.prototype.isEsculant = function() {
+	return this.esculant;
+};
+
+var EsculantFish = function(name, price) {
+	this.name = name;
+	this.price = price;
+	this.esculant = true;
+}
+EsculantFish.prototype = new Fish();
+
+// Other products
+var Rice = function(name, price) {
+	this.name = name;
+	this.price = price;
+};
+Rice.prototype = new Product();
+
+var Nori = function(name, price) {
+	this.name = name;
+	this.price = price;
+};
+Nori.prototype = new Product();
+
+var Sauce = function(name, price) {
+	this.name = name;
+	this.price = price;
+};
+Sauce.prototype = new Product();
+
+var Wasabi = function(name, price) {
+	this.name = name;
+	this.price = price;
+};
+Wasabi.prototype = new Product();
+// end Other products
 
 var product = new Product('', 12);
+var fish = new Fish('test', 10);
+var esfish = new EsculantFish('test', 10);
