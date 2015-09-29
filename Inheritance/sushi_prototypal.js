7aaa0dd3040ee89fe7c74d77b6ecf1f1e890fa8a
NatGovor <ngovor93@gmail.com>
2015-09-29 21:28:27 +0300

    added objects for prototypal implementation

--- Inheritance/sushi_prototypal.js
+++ Inheritance/sushi_prototypal.js
@@ -7,8 +7,8 @@ if (typeof Object.create !== 'function') {
 }
 
 // Basic product
-var basicProduct = {
-	name: 'Basic Product',
+var product = {
+	name: 'Product',
 	price: 0,
 	getName: function() {
 		return this.name;
@@ -19,7 +19,40 @@ var basicProduct = {
 };
 // end basic product
 
+var rice = Object.create(product);
+rice.name = 'Rice';
+rice.price = 2;
+
+var nori = Object.create(product);
+nori.name = 'Nori';
+nori.price = 2;
+
+var sauce = Object.create(product);
+sauce.name = 'Sauce';
+sauce.price = 3;
+
+var wasabi = Object.create(product);
+wasabi.name = 'Wasabi';
+wasabi.price = 1;
+
 var fish = Object.create(product);
+fish.name = 'Fugu';
+fish.price = 7;
+fish.esculant = false;
 fish.isEsculant = function() {
 	return this.esculant;
 };
+
+var salmon = Object.create(fish);
+salmon.name = 'Salmon';
+salmon.price = 5;
+salmon.esculant = true;
+
+var eel = Object.create(fish);
+eel.name = 'Eel';
+eel.price = 3;
+eel.esculant = true;
+
+var products = {
+		'Rice': rice
+	};
